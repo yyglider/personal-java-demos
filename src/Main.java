87bfaaa01f8fdb2693a3467bb1edc666fae83ba1
yaoyuan <yaoyuan0120@126.com>
2017-02-06 15:30:55 +0800

    personal java demos

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,10 @@
-public class Main {
-
-    public static void main(String[] args) {
-
-        String file = "ZzwtServer20160728.exe";
-        int index = file.lastIndexOf("\\");
-
-        System.out.println(file.substring(index+1));
-    }
-}
+//public class Main {
+//
+//    public static void main(String[] args) {
+//
+//        String file = "ZzwtServer20160728.exe";
+//        int index = file.lastIndexOf("\\");
+//
+//        System.out.println(file.substring(index+1));
+//    }
+//}
