0618fb5a3072133a6818d25aca1b7e3700203511
yaoyuan <yaoyuan0120@126.com>
2017-02-17 10:55:19 +0800

    personal java demos

---
+++ src/concurrency/book/atomic/AtomicIntegerDemo.java
@@ -0,0 +1,32 @@
+package concurrency.book.atomic;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * Created by yaoyuan on 2016/12/14.
+ */
+public class AtomicIntegerDemo {
+    static AtomicInteger i = new AtomicInteger();
+
+    public static class AddThread implements Runnable{
+        public void run() {
+            for (int k = 0; k < 10000; k++) {
+                i.incrementAndGet();
+            }
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread[] ts = new Thread[10];
+        for (int k = 0; k < 10; k++) {
+            ts[k] = new Thread((new AddThread()));
+        }
+        for (int k = 0; k < 10; k++) {
+            ts[k].start();
+        }
+        for (int k = 0; k < 10; k++) {
+            ts[k].join();
+        }
+        System.out.println(i);
+    }
+}
---
+++ src/concurrency/book/blockingqueue/ArrayBlockingQueueDemo.java
@@ -0,0 +1,13 @@
+package concurrency.book.blockingqueue;
+
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+
+/**
+ * Created by yaoyuan on 2016/12/7.
+ */
+public class ArrayBlockingQueueDemo {
+    ArrayBlockingQueue q = new ArrayBlockingQueue(100);
+    LinkedBlockingQueue lq = new LinkedBlockingQueue(100);
+
+}
